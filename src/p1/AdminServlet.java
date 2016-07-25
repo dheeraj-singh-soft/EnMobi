bbd13a7929695a7a0d68d690a80b7429a059850a
dheeraj <dheeraj.singh.soft@gmail.com>
2016-07-25 20:14:43 +0530

    first commit

---
+++ src/p1/AdminServlet.java
@@ -0,0 +1,73 @@
+package p1;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       String userName="";
+       String password="";
+       ServletContext context;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+@Override
+public void init(ServletConfig config) throws ServletException {
+	// TODO Auto-generated method stub
+	super.init(config);
+	    context = getServletContext();
+	    userName=context.getInitParameter("admin");
+	    password=context.getInitParameter("password");
+	
+}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		String adminUser=request.getParameter("userName");
+		String adminpassword=request.getParameter("userPassword");
+		writer.println(adminUser);
+		writer.println(adminpassword);
+		writer.println(userName);
+		writer.println(password);
+		if(adminUser.equals(userName) && adminpassword.equals(password))
+		{
+		RequestDispatcher requestDispatcher=request.getRequestDispatcher("welcome");	
+		requestDispatcher.forward(request,response);
+		}else {
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("index.html");	
+			requestDispatcher.forward(request,response);
+		}
+		String webInfPath = getServletConfig().getServletContext().getRealPath("WEB-INF");
+		File newFile = new File(webInfPath + "/fileName.xml");
+
+		//File file=new File(path);
+		writer.println("helloo");
+		writer.println(webInfPath);
+
+		
+		
+		
+	}
+
+}
